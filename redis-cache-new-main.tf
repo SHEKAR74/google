3434d56e7514b5e39025e2b46de2ec97a71519b6
SHEKAR74 <rajashekarreddy74@gmail.com>
2024-02-02 12:10:57 +0530

    Create redis-cache-new-main.tf

---
+++ redis-cache-new-main.tf
@@ -0,0 +1,74 @@
+# data "azurerm_resource_group" "guardian" {
+#   name = "rg-genaisppi-dev-westeu-001"
+# }
+
+# data "azurerm_subnet" "existing_redis_cache_subnet" {
+#   name                 = var.redis_cache_settings.redis_subnet_name
+#   virtual_network_name = var.redis_cache_settings.redis_vnet_name
+#   resource_group_name  = var.redis_cache_settings.vnet_rg_name
+# }
+
+data "azurerm_subnet" "existing_pe_subnet" {
+  name                 = var.redis_cache_settings.pe_subnet_name
+  virtual_network_name = var.redis_cache_settings.pe_vnet_name
+  resource_group_name  = var.redis_cache_settings.vnet_rg_name
+}
+
+# NOTE: the Name used for Redis needs to be globally unique
+//**********************************************************************************************
+resource "azurerm_redis_cache" "rc" {
+  name                          = var.redis_cache_settings.rc_name
+  location                      = var.location
+  resource_group_name           = data.azurerm_resource_group.guardian.name
+  capacity                      = var.redis_cache_settings.capacity
+  family                        = var.redis_cache_settings.family
+  sku_name                      = var.redis_cache_settings.sku_name
+  enable_non_ssl_port           = var.redis_cache_settings.enable_non_ssl_port
+  public_network_access_enabled = var.redis_cache_settings.public_network_access_enabled
+#   private_static_ip_address     = var.redis_cache_settings.private_static_ip_address
+#   subnet_id = data.azurerm_subnet.existing_redis_cache_subnet.id
+
+  redis_configuration {
+    #     rdb_backup_enabled            = true
+    #     rdb_backup_frequency          = 60
+    #     rdb_backup_max_snapshot_count = 1
+    #     rdb_storage_connection_string = "DefaultEndpointsProtocol=https;BlobEndpoint=${azurerm_storage_account.str_acc.primary_blob_endpoint};AccountName=${azurerm_storage_account.str_acc.name};AccountKey=${azurerm_storage_account.str_acc.primary_access_key}"
+  }
+}
+
+// Configure private endpoint for the Redis Cache
+//**********************************************************************************************
+resource "azurerm_private_endpoint" "redis_cache_private_endpoint" {
+  name                = "${azurerm_redis_cache.rc.name}-private-endpoint"
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.guardian.name
+  subnet_id           = data.azurerm_subnet.existing_pe_subnet.id
+
+  private_dns_zone_group {
+    name                 = "privatelink.redis.cache.windows.net"
+    private_dns_zone_ids = var.credis_cache_private_dns_zone_ids
+  }
+
+  private_service_connection {
+    name                           = azurerm_redis_cache.rc.name
+    private_connection_resource_id = azurerm_redis_cache.rc.id
+    subresource_names              = ["redisCache"]
+    is_manual_connection           = false
+  }
+  
+  lifecycle {
+    ignore_changes = [ private_dns_zone_group ]
+  }
+}
+
+
+
+//Storage Account Creation
+//**********************************************************************************************
+# resource "azurerm_storage_account" "str_acc" {
+#   name                     = "redissa74"
+#   resource_group_name      = "${data.azurerm_resource_group.guardian.name}"
+#   location                 = "${var.location}"
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
+# }
